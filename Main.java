3c67d4d7072b55b05c355eaf6745f98313a43b52
devSamuel29 <samueldflima@protonmail.com>
2023-06-15 00:17:42 -0300

    finaliza israel

--- Main.java
+++ Main.java
@@ -5,18 +5,33 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Objects;
 
 public class Main {
     public static void main(String[] args) {
-        final Path filePath = Paths.get("C:/Users/samue/IdeaProjects/untitled/src/arvore/base");
+        final Path filePathBase = Paths.get("C:/Users/samue/IdeaProjects/untitled/src/arvore/base");
+        final Path filePathTest = Paths.get("C:/Users/samue/IdeaProjects/untitled/src/arvore/test");
 
-        try (BufferedReader br = new BufferedReader(new FileReader(filePath.toFile()))) {
-            AvlTree<String> baseTxt = new AvlTree<>();
+        try {
+            BufferedReader documentBase = new BufferedReader(new FileReader(filePathBase.toFile()));
+            BufferedReader documentTest = new BufferedReader(new FileReader(filePathTest.toFile()));
 
-            for (String line = br.readLine(); line != null; line = br.readLine()) {
-                baseTxt.insert(line);
+            AvlTree<String> documentBaseTree = new AvlTree<>();
+            AvlTree<String> documentTestTree = new AvlTree<>();
+
+            for (String line = documentBase.readLine(); line != null; line = documentBase.readLine()) {
+                documentBaseTree.insert(line);
             }
-            System.out.println(baseTxt);
+
+            for (String line = documentTest.readLine(); line != null; line = documentTest.readLine()) {
+                documentTestTree.insert(line);
+            }
+
+            if(!Objects.equals(documentBaseTree.toString(), documentTestTree.toString())) {
+                System.out.println("modificado");
+            } else {
+                System.out.println("sem modificações");
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
